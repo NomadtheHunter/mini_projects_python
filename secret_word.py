4c51d7d5e7938077622209cff71242d2fec7d409
NomadtheHunter <felipenascimento.nomad@live.com>
2026-04-24 14:26:53 -0300

    commit

--- secret_word.py
+++ secret_word.py
@@ -2,7 +2,9 @@ secret_word = "Zoiao"
 guess = ""
 while guess != secret_word:
     guess = input("Guess a letter: ")
-    if guess == secret_word:
+    if guess in secret_word:
+        print("This letter is in the word")
+    elif guess in secret_word:
         print("Congratulations! You guessed the word!")
         break
     else:
