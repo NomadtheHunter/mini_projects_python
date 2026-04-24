6a7c0fdbe69b22551a97ad2aabbd8f824ba69f52
NomadtheHunter <felipenascimento.nomad@live.com>
2026-04-24 14:35:57 -0300

    fd

--- secret_word.py
+++ secret_word.py
@@ -2,8 +2,10 @@ secret_word = "Zoiao"
 guess = ""
 while guess != secret_word:
     guess = input("Guess a letter: ")
-    if guess in secret_word:
-        print("This letter is in the word")
+
+    if guess == secret_word:
+        print("Correct!")
+        break
     elif guess in secret_word:
         print("Congratulations! You guessed the word!")
         break
