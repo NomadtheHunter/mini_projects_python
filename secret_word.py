5656dd4be6b31aa876a094e934b41b8871f29615
MSI KATANA <felipenascimento.nomad@live.com>
2026-04-26 13:58:43 -0300

    Guess game improved

--- secret_word.py
+++ secret_word.py
@@ -14,8 +14,8 @@ while guess != secret_word:
         print("Correct!")
         break
 
-    if guess_count == guess_limit:
-        print("Game over")
+    if guess_count >= guess_limit:
+        print(f"Game over! The word was {secret_word}")
         break
     elif guess in secret_word:
         print("This letter is in the word!")
@@ -23,3 +23,6 @@ while guess != secret_word:
         print("Sorry, try again!")
 
     print(f"Attempts left: {guess_limit - guess_count}")
+
+    if guess_count == 2:
+        print("Hint: Nick name I had when I was a kid")
