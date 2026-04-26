c755c37f6fbff019cc849efc23d627a11a882bdf
MSI KATANA <felipenascimento.nomad@live.com>
2026-04-26 13:24:06 -0300

    Guess game improved

--- secret_word.py
+++ secret_word.py
@@ -1,12 +1,25 @@
 secret_word = "Zoiao"
 guess = ""
+guess_count = 0
+guess_limit = 3
+
+print("""Welcome to the secret word Game
+You got 3 tries to guess the word!""")
+
 while guess != secret_word:
     guess = input("Guess a letter: ")
+    guess_count += 1
 
     if guess == secret_word:
         print("Correct!")
         break
+
+    if guess_count == guess_limit:
+        print("Game over")
+        break
     elif guess in secret_word:
-        print("These letters are in the word!")
+        print("This letter are in the word!")
     else:
         print("Sorry, try again!")
+
+    print(f"Attempts left: {guess_limit - guess_count}")
